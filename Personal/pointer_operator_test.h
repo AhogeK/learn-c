bb4a6e81f4da28af433756d479a9d14b801011f8
AhogeK <ahogek@gmail.com>
2021-09-16 11:36:57 +0800

    feat(个人): 初识指针测试

---
+++ Personal/pointer_operator_test.h
@@ -0,0 +1,13 @@
+//
+// Created by ahogek on 9/16/21.
+//
+
+#ifndef LEARN_C_POINTER_OPERATOR_TEST_H
+#define LEARN_C_POINTER_OPERATOR_TEST_H
+
+/**
+ * 指针变量改变测试
+ */
+void pointer_op_test(void);
+
+#endif //LEARN_C_POINTER_OPERATOR_TEST_H
--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include "ChapterOne/fahr_celsius.h"
 #include "ChapterOne/1_4.h"
 #include "ChapterOne/1_5.h"
+#include "Personal/pointer_operator_test.h"
 
 int main()
 {
     printf("Hello, World!\n");
+    pointer_op_test();
 //    unitTest();
     return 0;
 }
