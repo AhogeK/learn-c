8db39969899c2bc9c751e4a1ddbdbef0c6b29f4e
AhogeK <ahogek@gmail.com>
2022-03-26 19:31:27 +0800

    test(1.4): add main function

--- ChapterOne/1_4.c
+++ ChapterOne/1_4.c
@@ -7,13 +7,17 @@
 #define LOWER  0    /* 温度表的下限 */
 #define UPPER 300   /* 温度表的上限 */
 #define STEP 20     /* 步长 */
+
 /**
  * 符号常量
  */
-void symbolConstants()
-{
+void symbolConstants() {
     int fahrenheit;
 
     for (fahrenheit = LOWER; fahrenheit <= UPPER; fahrenheit += STEP)
         printf("%3d %6.1f\n", fahrenheit, (5.0 / 9.0) * (fahrenheit - 32));
+}
+
+int main() {
+    symbolConstants();
 }
