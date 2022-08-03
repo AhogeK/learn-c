1c47751b0041bf4291a320c62287cf3311eae18b
AhogeK <ahogek@gmail.com>
2022-08-03 12:54:57 +0800

    style(第一章): 英文修正

--- ChapterOne/1_3_1.c
+++ ChapterOne/1_3_1.c
@@ -8,6 +8,6 @@
  * 打印华氏温度-摄氏温度对照表
  */
 int main() {
-    for (int fahr = 0; fahr <= 300; fahr = fahr + 20)
-        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    for (int fahrenheit = 0; fahrenheit <= 300; fahrenheit = fahrenheit + 20)
+        printf("%3d %6.1f\n", fahrenheit, (5.0 / 9.0) * (fahrenheit - 32));
 }
