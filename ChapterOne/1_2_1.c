4536b51a160abbdeaafaf3b90f46baa729da2eb3
AhogeK <ahogek@gmail.com>
2022-08-03 11:27:25 +0800

    refactor(第一章): gitignore更新，并修正英文

--- ChapterOne/1_2_1.c
+++ ChapterOne/1_2_1.c
@@ -5,12 +5,12 @@
 #include <stdio.h>
 
 /*
- * 当 fahr = 0, 20, ..., 300时, 分别
+ * 当 fahrenheit = 0, 20, ..., 300时, 分别
  * 打印华氏温度与摄氏温度对照表
  */
 int main() {
 
-    int fahr;
+    int fahrenheit;
     int celsius;
     int lower;
     int upper;
@@ -20,10 +20,10 @@ int main() {
     upper = 300; /* 温度表的上限 */
     step = 20;   /* 步长 */
 
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
-        fahr = fahr + step;
+    fahrenheit = lower;
+    while (fahrenheit <= upper) {
+        celsius = 5 * (fahrenheit - 32) / 9;
+        printf("%d\t%d\n", fahrenheit, celsius);
+        fahrenheit = fahrenheit + step;
     }
 }
