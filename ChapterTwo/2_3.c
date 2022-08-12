30fdf33f1994ffd2eafe73a6f17c295ea299a96b
AhogeK <ahogek@gmail.com>
2022-08-12 10:46:27 +0800

    docs(第二章): 常量

---
+++ ChapterTwo/2_3.c
@@ -0,0 +1,11 @@
+//
+// Created by Ahoge Knight on 2022/8/12.
+//
+
+#include <string.h>
+#include <printf.h>
+
+int main() {
+    char const *str = "Hello, World";
+    printf("%lu\n", strlen(str));
+}
