e4628a5189ae516a82349976c1901eda29ba6b5d
AhogeK <ahogek@gmail.com>
2022-10-12 15:46:37 +0800

    docs(第二章): strlen 测试

---
+++ ChapterTwo/strlen_test.c
@@ -0,0 +1,10 @@
+//
+// Created by Ahoge Knight on 2022/10/12.
+//
+#include <string.h>
+#include <printf.h>
+
+int main() {
+    char s[] = "Hello World!";
+    printf("'Hello World!' length: %lu", strlen(s));
+}
