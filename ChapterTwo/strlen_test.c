6c39391bc259fc09bd4f3212347a3acc64fcd0ed
AhogeK <ahogek@gmail.com>
2022-10-12 15:55:16 +0800

    docs(第二章): strlen 测试

--- ChapterTwo/strlen_test.c
+++ ChapterTwo/strlen_test.c
@@ -1,8 +1,17 @@
 //
 // Created by Ahoge Knight on 2022/10/12.
 //
-#include <string.h>
 #include <printf.h>
+
+unsigned long strlen(const char s[]) {
+    int i;
+
+    i = 0;
+
+    while (s[i] != '\0')
+        ++i;
+    return i;
+}
 
 int main() {
     char s[] = "Hello World!";
