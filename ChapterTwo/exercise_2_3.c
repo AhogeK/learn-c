606e5dd15f1774abcadba7b2fdafb25aa8517c7a
AhogeK <ahogek@gmail.com>
2022-10-13 13:29:10 +0800

    feat(第二章): 2.3 练习

---
+++ ChapterTwo/exercise_2_3.c
@@ -0,0 +1,65 @@
+//
+// Created by Ahoge Knight on 2022/10/13.
+//
+
+#include <string.h>
+#include <printf.h>
+#include <math.h>
+#include <stdlib.h>
+
+int htoi(const char s[]) {
+    unsigned long size = strlen(s);
+    if (size == 2) {
+        printf("%s 不是十六进制字符串", s);
+        exit(0);
+    }
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        int dec = 0;
+        if ((i == 0 && s[i] !='0') || (i == 1 && s[i] != 'x' && s[i] != 'X')) {
+            printf("%s 不是十六进制字符串", s);
+            exit(0);
+        }  else if (i > 1) {
+            if (!(s[i] >= '0' && s[i] <= '9' || s[i] >= 'a' && s[i] <= 'f' || s[i] >= 'A' && s[i] <= 'F')) {
+                printf("%s 不是十六进制字符串", s);
+                exit(0);
+            }
+            // 开始计算十六进制
+            if (s[i] >= '0' && s[i] <= '9') {
+                dec = s[i] - '0';
+            } else {
+                char c = (char) toupper(s[i]);
+                switch (c) {
+                    case 'A':
+                        dec = 10;
+                        break;
+                    case 'B':
+                        dec = 11;
+                        break;
+                    case 'C':
+                        dec = 12;
+                        break;
+                    case 'D':
+                        dec = 13;
+                        break;
+                    case 'E':
+                        dec = 14;
+                        break;
+                    case 'F':
+                        dec = 15;
+                        break;
+                    default:
+                        // do nothing
+                        break;
+                }
+            }
+            sum += dec * (int) pow(16, i - 2);
+        }
+    }
+    return sum;
+}
+
+int main() {
+    char s[] = "0xFFFF";
+    printf("%s转换为十进制为：%d", s, htoi(s));
+}
