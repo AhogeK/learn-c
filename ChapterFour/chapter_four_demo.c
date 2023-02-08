adfd9e842026104471ac00fad73a51e0abb39faf
AhogeK <ahogek@gmail.com>
2023-02-08 12:04:36 +0800

    C预处理器

---
+++ ChapterFour/chapter_four_demo.c
@@ -0,0 +1,24 @@
+//
+// Created by Ahoge Knight on 2023/2/7.
+//
+
+#include <stdio.h>
+
+#define STR_PRINT(x) #x
+#define STR_CONCAT(x, y) x ## _ ## y
+
+#if !defined(SIZE)
+#define SIZE 100
+#endif
+
+int main() {
+    printf(STR_PRINT(This is a string without double quotes));
+    int x_y = 2050;
+    printf("\n%d\n", STR_CONCAT(x, y));
+
+#if SIZE > 99
+    printf("For 100 plus elements.\n");
+#else
+    printf("For less then 100 elements.\n");
+#endif
+}
