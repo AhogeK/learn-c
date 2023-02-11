8f058df0c13d87f04d90d838714eeb95df1edca6
AhogeK <ahogek@gmail.com>
2023-02-11 14:11:13 +0800

    Dynamic Memory Allocation in C using malloc(), calloc(), free() and realloc()

--- ChapterFive/chapter_five_demo.c
+++ ChapterFive/chapter_five_demo.c
@@ -3,6 +3,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void swap(int *a, int *b);
 
@@ -46,6 +47,138 @@ void pointer() {
     // int *wildPointer;
     // will segmentation fault
     // printf("%d\n", *wildPointer);
+}
+
+void mallocDemo() {
+    // This pointer will hold the base address of the block created
+    int *ptr;
+    int n, i;
+
+    // Get the number of elements for the array;
+    printf("Enter number of elements:");
+    // scanf("%d", &n);
+    n = 10;
+    printf("Entered number of elements: %d\n", n);
+
+    // Dynamically allocate memory using malloc()
+    ptr = (int *) malloc(n * sizeof(int));
+
+    // Check if the memory has been successfully allocated by malloc or not
+    if (ptr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    } else {
+        // Memory has been successfully allocated
+        printf("Memory successfully allocated using malloc.\n");
+
+        // Get the elements of the array
+        for (i = 0; i < n; ++i) {
+            ptr[i] = i + 1;
+        }
+
+        // Print the elements of the array
+        printf("The elements of the array are: ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", ptr[i]);
+        }
+    }
+    free(ptr);
+}
+
+void callocDemo() {
+    // This pointer will hold the
+    // base address of the block created
+    int *ptr;
+    int n, i;
+
+    // Get the number of elements for the array
+    n = 5;
+    printf("Enter number of elements: %d\n", n);
+
+    // Dynamically allocate memory using calloc()
+    ptr = (int *) calloc(n, sizeof(int));
+
+    // Check if the memory has been successfully
+    // allocated by calloc or not
+    if (ptr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    } else {
+
+        // Memory has been successfully allocated
+        printf("Memory successfully allocated using calloc.\n");
+
+        // Get the elements of the array
+        for (i = 0; i < n; ++i) {
+            ptr[i] = i + 1;
+        }
+
+        // Print the elements of the array
+        printf("The elements of the array are: ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", ptr[i]);
+        }
+    }
+    free(ptr);
+}
+
+void reallocDemo() {
+    // This pointer will hold the
+    // base address of the block created
+    int *ptr;
+    int n, i;
+
+    // Get the number of elements for the array
+    n = 5;
+    printf("Enter number of elements: %d\n", n);
+
+    // Dynamically allocate memory using calloc()
+    ptr = (int *) calloc(n, sizeof(int));
+
+    // Check if the memory has been successfully
+    // allocated by malloc or not
+    if (ptr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    } else {
+
+        // Memory has been successfully allocated
+        printf("Memory successfully allocated using calloc.\n");
+
+        // Get the elements of the array
+        for (i = 0; i < n; ++i) {
+            ptr[i] = i + 1;
+        }
+
+        // Print the elements of the array
+        printf("The elements of the array are: ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", ptr[i]);
+        }
+
+        // Get the new size for the array
+        n = 10;
+        printf("\n\nEnter the new size of the array: %d\n", n);
+
+        // Dynamically re-allocate memory using realloc()
+        ptr = realloc(ptr, n * sizeof(int));
+
+        // Memory has been successfully allocated
+        printf("Memory successfully re-allocated using realloc.\n");
+
+        // Get the new elements of the array
+        for (i = 5; i < n; ++i) {
+            ptr[i] = i + 1;
+        }
+
+        // Print the elements of the array
+        printf("The elements of the array are: ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", ptr[i]);
+        }
+
+        free(ptr);
+    }
 }
 
 int main() {
@@ -96,6 +229,12 @@ int main() {
         printf("%d ", *(ptr + i));
     }
     printf("\n\n\n");
+
+    // Dynamic Memory Allocation in C using malloc(), calloc(), free() and realloc()
+    mallocDemo();
+    callocDemo();
+    reallocDemo();
+    return 0;
 }
 
 /**
