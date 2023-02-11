98755392325247c14b0fe683e8278384cc7d7e54
AhogeK <ahogek@gmail.com>
2023-02-11 11:42:33 +0800

    Swapping two numbers using Pointer

--- ChapterFive/chapter_five_demo.c
+++ ChapterFive/chapter_five_demo.c
@@ -4,6 +4,8 @@
 
 #include <stdio.h>
 
+void swap(int *a, int *b);
+
 void pointer() {
     int var = 10;
     int *ptr;
@@ -44,4 +46,29 @@ void pointer() {
 
 int main() {
     pointer();
+
+    // Pointers as Function Argument in C
+    int m = 10;
+    int n = 20;
+    printf("\n\nm = %d\n", m);
+    printf("n = %d\n", n);
+    // passing address of m and n to the swap function
+    swap(&m, &n);
+    printf("After Swapping: \n\n");
+    printf("m = %d\n", m);
+    printf("n = %d\n", n);
+    return 0;
+}
+
+/**
+ * pointer 'a' and pointer 'b' holds and points to  the address of 'm' and 'n'
+ *
+ * @param a to swap b
+ * @param b  to swap a
+ */
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
