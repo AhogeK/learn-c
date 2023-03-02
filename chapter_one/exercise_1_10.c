722c8ad2eaf880987de1a31cd33b453f9b6d2ca0
AhogeK <ahogek@gmail.com>
2023-03-02 14:33:58 +0800

    refactor: 练习1-10

---
+++ chapter_one/exercise_1_10.c
@@ -0,0 +1,31 @@
+//
+// Created by Ahoge Knight on 2023/3/2.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int c;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\t')
+        {
+            printf("\\t");
+        }
+        else if (c == '\b')
+        {
+            printf("\\b");
+        }
+        else if (c == '\\')
+        {
+            printf("\\\\");
+        }
+        else
+        {
+            putchar(c);
+        }
+    }
+    return 0;
+}
