0d5374453674c144f03fd00b77ab02e1f73b2e72
AhogeK <ahogek@gmail.com>
2023-03-17 12:33:27 +0800

    refactor: 练习1-4

---
+++ chapter_one/exercise_1_4.c
@@ -0,0 +1,30 @@
+//
+// Created by Ahoge Knight on 2023/3/17.
+//
+
+#include <printf.h>
+
+int main()
+{
+    int celsius;
+    int fahrenheit;
+    int lower;
+    int upper;
+    int step;
+
+    lower = -30; /* 温度的下限 */
+    upper = 30; /* 温度的上限 */
+    step = 10; /* 每行的温差（步长） */
+
+    printf("%-6s\t%-6s\n", "摄氏度", "华氏度"); /* 标题 */
+
+    celsius = lower;
+    while (celsius <= upper)
+    {
+        fahrenheit = 9 * celsius / 5 + 32;
+        printf("%-6d\t%-6d\n", celsius, fahrenheit);
+        celsius = celsius + step;
+    }
+
+    return 0;
+}
